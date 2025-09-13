1d80e0d3cba77be3a7e70b3b910917570f7fb170
Entabot <bort.anovia56@gmail.com>
2025-09-13 09:38:09 +0700

    First commit.

---
+++ Python/CS50/calculator.py
@@ -0,0 +1,8 @@
+x = input("What's X: ")
+y = input("What's y: ")
+
+z = round(float(x) + float(y))
+
+#You can use \ to force quotation marks inside print's quotation marks
+#Example: The result is "z"
+print(f"The result is \"{z}\"")
